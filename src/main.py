f51c393cc6ecac6025803d668b6508b4363dabcc
romainlief <romain.liefferinckx@ulb.be>
2026-02-01 14:42:05 +0100

    rossler

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from sprott import SprottAttractor
 from threescroll import ThreeScrollAttractor
 from rabinovichfabrikant import RabinovichFabrikantAttractor
 from halvorsen import HalvorsenAttractor
+from rossler import RosslerAttractor
 
 from simulation import Simulation
 
@@ -20,12 +21,13 @@ def main():
     # attractor = SprottAttractor()
     # attractor = ThreeScrollAttractor()
     #attractor = RabinovichFabrikantAttractor()
-    attractor = HalvorsenAttractor()
+    #attractor = HalvorsenAttractor()
+    attractor = RosslerAttractor()
 
     sim = Simulation(attractor)
 
-    initial_state = (-1.48, -1.51, 2.04)  # Initial condition x, y, z
-    dt = 0.001
+    initial_state = (10.0, 0.0, 10.0)  # Initial condition x, y, z
+    dt = 0.006
     steps = 200000
 
     states = sim.run(initial_state, dt, steps)
@@ -34,7 +36,7 @@ def main():
     sim.animate(
         states,
         interval=30,
-        steps_per_frame=300,
+        steps_per_frame=100,
         color_speed=10.0,
         cmap_name="hsv",
         line_width=1.0,
