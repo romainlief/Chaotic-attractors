19a90297f5efefbdb5593da356e665599234a5d0
romainlief <romain.liefferinckx@ulb.be>
2026-02-03 14:38:50 +0100

    arneodo

---
+++ src/arneodo.py
@@ -0,0 +1,16 @@
+from AAttractor.Aattractor import AAttractor
+
+
+class ArneodoAttractor(AAttractor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def derivatives(self, state):
+        x, y, z = state
+        a = -5.5
+        b = 3.5
+        c = -1.0
+        dx = y
+        dy = z
+        dz = -a * x - b * y - z + c * x * x * x
+        return dx, dy, dz
--- src/main.py
+++ src/main.py
@@ -10,45 +10,50 @@ from rossler import RosslerAttractor
 from dadras import DadrasAttractor
 from langford import LangfordAttractor
 from thomas import ThomasAttractor
+from arneodo import ArneodoAttractor
 from simulation import Simulation
 
 
 def main():
-    # attractor = ChenAttractor()
-    # attractor = ChuaAttractor()
-    # attractor = ChenLeeAttractor()
-    # attractor = FourWingsAttractor()
-    # attractor = SprottAttractor()
-    # attractor = ThreeScrollAttractor()
-    # attractor = RabinovichFabrikantAttractor()
-    # attractor = HalvorsenAttractor()
-    # attractor = RosslerAttractor()
-    # attractor = DadrasAttractor()
-    # attractor = LangfordAttractor()
-    attractor = ThomasAttractor()
+    try:
+        # attractor = ChenAttractor()
+        # attractor = ChuaAttractor()
+        # attractor = ChenLeeAttractor()
+        # attractor = FourWingsAttractor()
+        # attractor = SprottAttractor()
+        # attractor = ThreeScrollAttractor()
+        # attractor = RabinovichFabrikantAttractor()
+        # attractor = HalvorsenAttractor()
+        # attractor = RosslerAttractor()
+        # attractor = DadrasAttractor()
+        # attractor = LangfordAttractor()
+        # attractor = ThomasAttractor()
+        attractor = ArneodoAttractor()
 
-    sim = Simulation(attractor)
+        sim = Simulation(attractor)
 
-    initial_state = (2.0, -1.0, 0.5)  # Initial condition x, y, z
-    dt = 0.03
-    steps = 300_000
+        initial_state = (1.0, 1.0, 0.0)  # Initial condition x, y, z
+        dt = 0.006
+        steps = 300_000
 
-    states = sim.run(initial_state, dt, steps)
-    # states = states[50000:]  # Discard initial transient for rabinovich-fabrikant
+        states = sim.run(initial_state, dt, steps)
+        # states = states[50000:]  # Discard initial transient for rabinovich-fabrikant
 
-    sim.animate(
-        states,
-        interval=30,
-        steps_per_frame=300,
-        color_speed=10.0,
-        cmap_name="hsv",
-        line_width=1.0,
-        rotate_camera=True,
-        rotation_speed_deg=1.5,
-        elevation_deg=5.0,
-        rotate_y=False,
-        rotation_speed_y_deg=0.4,
-    )
+        sim.animate(
+            states,
+            interval=30,
+            steps_per_frame=200,
+            color_speed=10.0,
+            cmap_name="hsv",
+            line_width=1.0,
+            rotate_camera=True,
+            rotation_speed_deg=1.5,
+            elevation_deg=5.0,
+            rotate_y=False,
+            rotation_speed_y_deg=0.4,
+        )
+    except KeyboardInterrupt:
+        print("Simulation interrupted.")
 
 
 if __name__ == "__main__":
